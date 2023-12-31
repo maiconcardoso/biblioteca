86e7e3b5734ea485b546c41a34b2e1c2ef4a21f2
Maicon Cardoso <maiconscardoso@hotmail.com>
2023-12-31 09:17:43 -0300

    Organizando o código de conexão com o banco de dados

--- src/main/java/dao/ConexaoDao.java
+++ src/main/java/dao/ConexaoDao.java
@@ -2,19 +2,33 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConexaoDao {
-	Connection con = null;
 	
-	public Connection conexaoBD() {
+	/**
+	 * Módulo de Conexão
+	*/
+	
+	// Parâmetros de conexão
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://localhost:3306/biblioteca?useTimezone=true&serverTimezone=UTC";
+	private String username = "root";
+	private String password = "mysql@123";
+	
+	// Método de conexão
+	public Connection connection () throws SQLException, ClassNotFoundException {
+		Connection connection = null;
 		
 		try {
-			String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
-			con = DriverManager.getConnection(url);
-		} catch(Exception e) {
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Conexão bem-sucedida");
+			return connection;
+			
+		} catch(SQLException e) {
 			System.err.println(e.getMessage());
+			return null;
 		}
-		return null;
 	}
-
 }
