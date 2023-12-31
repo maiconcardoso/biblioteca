80523528a7791bbce92b75bc3eea931f6126b9d8
Maicon Cardoso <maiconscardoso@hotmail.com>
2023-12-31 12:49:56 -0300

    Criação do metodo de listar livros

--- src/main/java/dao/LivroDao.java
+++ src/main/java/dao/LivroDao.java
@@ -2,34 +2,55 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import dto.LivroDto;
 
 public class LivroDao {
 	
 	private Connection connection;
-	private ConexaoDao conexaoDao;
 	private PreparedStatement pst;
+	private ResultSet rs;
+	List<LivroDto> lista = new ArrayList<>();
 	
-	// Injeção de Dependência
-	public LivroDao(ConexaoDao conexaoDao) {
-		this.conexaoDao = conexaoDao;
-	}
+	public LivroDao() {};
+	
 	
 	
 	public void create(LivroDto novoLivro) throws SQLException, ClassNotFoundException{
-		String createSQL = "INSERT INTO livro (name) VALUES (?)";
+		String createSQL = "INSERT INTO livro (nome) VALUES (?)";
 		try {
-			connection = conexaoDao.connection();
+			connection = new ConexaoDao().connection();
 			pst = connection.prepareStatement(createSQL);
 			pst.setString(1, novoLivro.getName());
 			pst.executeUpdate();
+			pst.close();
 			
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
-		} finally {
-			pst.close();
+		} 
+	}
+	
+	public List<LivroDto> readAll() throws ClassNotFoundException {
+		String readSQL = "SELECT * FROM livro";
+		try {
+			connection = new ConexaoDao().connection();
+			pst = connection.prepareStatement(readSQL);
+			rs = pst.executeQuery();
+			while(rs.next()) {
+				LivroDto livroDto = new LivroDto();
+				livroDto.setId(rs.getInt("id"));
+				livroDto.setName(rs.getString("nome"));
+				lista.add(livroDto);
+			}
+			return lista;
+			
+		} catch(SQLException e) {
+			System.err.println(e.getMessage());	
+			return null;
 		}
 	}
 }
